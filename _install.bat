ab7f2b9818934658faaaed24a3bd648b75bd0b34
alexo <the.onga@gmail.com>
2023-08-20 02:42:29 +1000

    feat: Convert from pipenv => poetry

--- _install.bat
+++ _install.bat
@@ -1,2 +1,6 @@
-python -m pip install pipenv
-pipenv install
+:: install pipx, enables `poetry.exe` to be in a virtualenv lol
+python -m pip install pipx
+pipx install poetry
+:: poetry can't find python3.8 beacuse its called python.exe. rough.
+poetry env use C:/Python38/python.exe
+poetry install
--- _run.bat
+++ _run.bat
@@ -1,2 +1,2 @@
-:: this starts pipenv and uses start pythonw to kill the black command prompt
-start /min pipenv run start pythonw main.pyw
+:: this starts poetry and uses start pythonw to kill the black command prompt
+start /min poetry run start pythonw main.pyw
