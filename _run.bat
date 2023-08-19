ca194e63acdbeaaf7ffc4417e15018c5b5e3d118
alexo <the.onga@gmail.com>
2023-08-20 01:34:49 +1000

    feat: less obtrusive opener

--- _run.bat
+++ _run.bat
@@ -1,2 +1,2 @@
-rem this starts pipenv and uses start pythonw to kill the black command prompt
-start pipenv run start pythonw main.pyw
+:: this starts pipenv and uses start pythonw to kill the black command prompt
+start /min pipenv run start pythonw main.pyw
